d84d3d0f1dbe5c656e43aae7423edb6c1d3deb98
nathaliewillemse21 <njpwillemse69@gmail.com>
2024-03-05 14:27:26 +0200

    model

---
+++ model/index.js
@@ -0,0 +1,38 @@
+console.log('Annyeonghaseyo');
+import { Users } from './Users.js';
+
+import { Library } from './Products.js';
+
+let users = new Users();
+let books = new Library();
+export { users, books };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
